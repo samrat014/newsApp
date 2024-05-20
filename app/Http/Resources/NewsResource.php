2551de5b8b76c11155b8f8f4ec5f66bfbae2aa55
karki <samratkarki2019@gmail.com>
2024-05-20 12:53:30 +0545

    add : resources

---
+++ app/Http/Resources/NewsResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class NewsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'category' => $this->category,
+            'title' => $this->title,
+            'content' => $this->content,
+            'image' => $this->image,
+
+            'comment' => CommentResource::collection($this->comment),
+            'user' => UserResource::make($this->user),
+        ];
+    }
+}
