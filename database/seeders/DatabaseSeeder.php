982a7ffe77046732e09b67808fe64530421b8edf
karki <samratkarki2019@gmail.com>
2024-05-20 13:56:17 +0545

    add : seed for news

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\News;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        News::create([
+            "category" => "politics",
+            "title" => "title",
+            "content" => "this is politics content",
+            "user_id" => 1
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        News::create([
+            "category" => "health",
+            "title" => "health",
+            "content" => "this is health content",
+            "user_id" => 2
+        ]);
     }
 }
