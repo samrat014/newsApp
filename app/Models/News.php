554a72daca925d28b79669d06a2ff474a44aba67
karki <samratkarki2019@gmail.com>
2024-05-20 12:34:29 +0545

    add : relation

--- app/Models/News.php
+++ app/Models/News.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class News extends Model
 {
@@ -23,4 +25,14 @@ class News extends Model
         'updated_at',
     ];
 
+
+    public function comment() : HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
